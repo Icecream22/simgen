b64e2c74bbe1bb7b8233dbe52f05859e0efc6e8c
zzf82018@gmail.com <zzf82018@gmail.com>
2017-08-11 22:18:56 +0800

    add product details

---
+++ Controllers/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simgenfrontend.Controllers
+{
+    public class Products : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
