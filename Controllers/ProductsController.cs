3704496411ce3b412581ed4b0fad21ea1d18aa77
Icecream22 <851334001@qq.com>
2017-08-21 16:55:20 +0800

    2017-08-21

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -13,54 +13,62 @@ namespace simgenfrontend.Controllers
             return View ();
         }
 
-        public ActionResult Details(int id)
+        public ActionResult Select()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Details()   /*为什么是Details(int id)，有问题，把其内容删除*/
         {
             return View ();
         }
 
-        public ActionResult Create()
-        {
-            return View ();
-        } 
+       
 
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try {
-                return RedirectToAction ("Index");
-            } catch {
-                return View ();
-            }
-        }
-        
-        public ActionResult Edit(int id)
-        {
-            return View ();
-        }
+        //public ActionResult Create()
+        //{
+        //    return View ();
+        //} 
 
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try {
-                return RedirectToAction ("Index");
-            } catch {
-                return View ();
-            }
-        }
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try {
+        //        return RedirectToAction ("Index");
+        //    } catch {
+        //        return View ();
+        //    }
+        //}
 
-        public ActionResult Delete(int id)
-        {
-            return View ();
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return View ();
+        //}
 
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try {
-                return RedirectToAction ("Index");
-            } catch {
-                return View ();
-            }
-        }
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try {
+        //        return RedirectToAction ("Index");
+        //    } catch {
+        //        return View ();
+        //    }
+        //}
+
+        //public ActionResult Delete(int id)
+        //{
+        //    return View ();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try {
+        //        return RedirectToAction ("Index");
+        //    } catch {
+        //        return View ();
+        //    }
+        //}
     }
 }
